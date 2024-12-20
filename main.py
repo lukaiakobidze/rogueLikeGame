5b30947bcb74886e6ee9c46b4e63fae01e88131c
Luka Iakobidze <lukaiakobidze@gmail.com>
2024-12-20 05:14:11 +0400

    big update: added functions for generating rectangular rooms and line tunnels. made simple procedural generating for the rooms connecting them with the tunnels. spawning the player in the first room and npc in random room.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tcod
 from engine import Engine
 from input_handlers import EventHandler
-from game_map import GameMap
 from entity import Entity
+from proc_gen import generate_dungeon
 
 
 
@@ -12,6 +12,10 @@ def main() -> None:
 
     map_width = 80
     map_height = 45
+    
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -23,7 +27,7 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {player, npc}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height, room_max_size, room_min_size, max_rooms, player, npc)
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
